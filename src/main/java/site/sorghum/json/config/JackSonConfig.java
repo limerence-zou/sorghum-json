48adcfdc6e7668f34ca13b1e6a9072a42911506d
king <jxphwy@gmail.com>
2021-03-26 14:22:55 +0800

    修复兼容

--- src/main/java/site/sorghum/json/config/JackSonConfig.java
+++ src/main/java/site/sorghum/json/config/JackSonConfig.java
@@ -2,7 +2,6 @@ package site.sorghum.json.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 /**
